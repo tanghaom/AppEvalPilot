eb168b963473dc4e42744319e07e8ccdde1c0238
tanghaom <438113369@qq.com>
2025-04-16 10:24:11 +0800

    feat: Enhance task handling in eval_runner to support work_path
    1. Add support for processing tasks with a "work_path" key in `eval_runner.py`, allowing for more flexible task execution.
    2. Implement a delay after starting tasks with "work_path" to ensure proper initialization.
    3. Update window termination logic to include additional processes when "work_path" is used, ensuring comprehensive cleanup of resources.

--- appeval/roles/eval_runner.py
+++ appeval/roles/eval_runner.py
@@ -364,12 +364,18 @@ class AppEvalRole(Role):
                 if "test_cases" in task_info:
                     if "url" in task_info:
                         pid = await start_windows(task_info["url"])
+                    elif "work_path" in task_info:
+                        pid = await start_windows(work_path=task_info["work_path"])
+                        await asyncio.sleep(20)
                     await asyncio.sleep(10)
 
                     task_id_case_number = len(test_cases[task_id]["test_cases"])
                     await self.execute_batch_check(task_id, task_id_case_number, task_info)
                     if "url" in task_info:
                         await kill_windows(["Chrome"])
+                        await kill_process(pid)  # ensure the process is killed
+                    elif "work_path" in task_info:
+                        await kill_windows(["Chrome", "cmd", "npm", "projectapp", "Edge"])
                         await kill_process(pid)  # ensure the process is killed
 
             # 4. Read results
