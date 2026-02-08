84a8cd789c308a8e212df83b70531e993dd1b5b2
stellahong <stellahong@fuzhi.ai>
2026-02-08 13:35:16 +0800

    Remove test_run.sh from version control

--- test_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Quick test script
-export DISPLAY=:99
-export CONFIG_ROOT=/root/.metagpt
-export BROWSER=/usr/bin/google-chrome
-# Fix Anaconda libffi conflict for pyatspi/gi
-export LD_PRELOAD=/lib/x86_64-linux-gnu/libffi.so.7
-source venv/bin/activate
-python main.py --platform Linux --max_iters 5
